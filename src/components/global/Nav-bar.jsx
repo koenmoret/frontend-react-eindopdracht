38b4242e8363fba5072ab0604b61d7cdc8fa78dd
koenmoret <koen@moret.info>
2024-04-17 20:24:01 +0200

    Navbar toggle dashboard/news/login with authentication

--- src/components/global/Nav-bar.jsx
+++ src/components/global/Nav-bar.jsx
@@ -2,19 +2,32 @@ import './Nav-bar.css';
 import {FaShoppingCart} from "react-icons/fa";
 import {NavLink} from "react-router-dom";
 
+import { AuthContext } from '../../context/AuthContext.jsx';
+import {useContext} from "react";
+
+
 
 // eslint-disable-next-line react/prop-types
 function NavBar({setClass}) {
+
+    const { isAuth } = useContext(AuthContext);
 
     return (
         <>
             <div className="header--nav">
                 <nav className={`${setClass}`}>
                     <ul>
-                        <li><NavLink to="/" className={({isActive}) => isActive ? 'nav-item active-link' : 'nav-item default-link'}>Home</NavLink></li>
-                        <li><NavLink to="/products" className={({isActive}) => isActive ? 'nav-item active-link' : 'nav-item default-link'}>Producten</NavLink></li>
-                        <li>Nieuws</li>
-                        <li>Login</li>
+                        <li><NavLink to="/"
+                                     className={({isActive}) => isActive ? 'nav-item active-link' : 'nav-item default-link'}>Home</NavLink>
+                        </li>
+                        <li><NavLink to="/products"
+                                     className={({isActive}) => isActive ? 'nav-item active-link' : 'nav-item default-link'}>Producten</NavLink>
+                        </li>
+                        {isAuth && <><li><span className="nav-item">Dashboard</span></li>
+                            <li><span className="nav-item">Nieuws</span></li>
+                        </>}
+                        {isAuth ? <li><span className="nav-item">Logout</span></li> :
+                            <li><span className="nav-item">Login</span></li>}
                         <li>
                             <span className="fa-shopping-cart">
                                <FaShoppingCart/>
